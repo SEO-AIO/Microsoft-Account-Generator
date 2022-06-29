0c02a806633395f6340beed2c9a0de71952cea35
b8ff <88239635+b8ff@users.noreply.github.com>
2022-06-29 15:06:05 +0200

    Updated PotatoGen.py
    PotatoGen v2 Changelog:
     - Made account generation faster
     - Fixed some errors
     - Improved some features
     - Updated Selenium to version 4.3.0
     - Changed Chrome Driver window size

--- Potato Gen/PotatoGen.py
+++ Potato Gen/PotatoGen.py
@@ -5,6 +5,7 @@ import undetected_chromedriver as ucd
 from random_word import RandomWords
 from time import sleep
 import ctypes
+import msvcrt
 import sys
 import os
 
@@ -12,12 +13,12 @@ def log(step):
     print(step)
 
 def setup():
-    ctypes.windll.kernel32.SetConsoleTitleW("PotatoGen v1")
+    ctypes.windll.kernel32.SetConsoleTitleW("PotatoGen v2")
     os.system("cls")
     os.system("color 03")
     print("""
          ______   ______    ______   ______    ______   ______    ______    ______    __   __    
-        /\  == \ /\  __ \  /\__  _\ /\  __ \  /\__  _\ /\  __ \  /\  ___\  /\  ___\  /\ "-.\ \   
+        /\ === \ /\  __ \  /\__  _\ /\  __ \  /\__  _\ /\  __ \  /\  ___\  /\  ___\  /\ "-.\ \   
         \ \  _-/ \ \ \/\ \ \/_/\ \/ \ \  __ \ \/_/\ \/ \ \ \/\ \ \ \ \__ \ \ \  __\  \ \ \-.  \  
          \ \_\    \ \_____\   \ \_\  \ \_\ \_\   \ \_\  \ \_____\ \ \_____\ \ \_____\ \ \_\\\\"\_\ 
           \/_/     \/_____/    \/_/   \/_/\/_/    \/_/   \/_____/  \/_____/  \/_____/  \/_/ \/_/ 
@@ -38,7 +39,8 @@ class PotatoGen:
         self.DRIVER = None
         self.DRIVERDIRECTORY = "chromedriver.exe"
         self.DRIVEROPTIONS = None
-        self.DRIVERVERSION = 102
+        self.PREFERENCES = {"credentials_enable_service": False, "profile.password_manager_enabled": False}
+        self.DRIVERVERSION = 103
 
         self.MICROSOFTSIGNUP = "https://signup.live.com/signup?wa=wsignin1.0&rpsnv=13&ct=1656322628&rver=7.0.6737.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fnlp%3d1%26RpsCsrfState%3d84195185-8c5b-4818-2f7d-dc10a1b164aa&id=292841&aadredir=1&whr=outlook.com&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=90015&contextid=7CD0ACC3A36D208A&bk=1656322628&uiflavor=web&lic=1&mkt=EN-EN&lc=1040&uaid=304f33a463d341388d71ab5068be748c"
 
@@ -49,6 +51,9 @@ class PotatoGen:
         self.LASTNAME_INPUT = None
         self.FULLNAME_INPUT = None
         self.PASSWORD_INPUT = None
+
+        self.NAME_LENGTH = None
+        self.LASTNAME_LENGTH = None
 
         self.COUNTRY_INPUT = None
         self.BIRTHMONTH_INPUT = None
@@ -62,14 +67,18 @@ class PotatoGen:
     def credentials(self):
         log("\nGenerating credentials")
 
-        while True:
-            try:
-                self.NAME_INPUT = self.WORDS.get_random_word(hasDictionaryDef="true", minLength=6, maxLength=6).title()
-                self.LASTNAME_INPUT = self.WORDS.get_random_word(hasDictionaryDef="true", minLength=6, maxLength=6).title()
-                break
-            except (Exception,):
-                sleep(2)
-                continue
+        self.NAME_LENGTH = randint(6, 9)
+        self.LASTNAME_LENGTH = randint(6, 9)
+
+        try:
+            self.NAME_INPUT = self.WORDS.get_random_word(hasDictionaryDef="true", minLength=self.NAME_LENGTH, maxLength=self.NAME_LENGTH).title()
+            self.LASTNAME_INPUT = self.WORDS.get_random_word(hasDictionaryDef="true", minLength=self.LASTNAME_LENGTH, maxLength=self.LASTNAME_LENGTH).title()
+        except (Exception,):
+            log("\nPotatoGen could not generate credentials. This is probably caused by a failed connection to the dictionary website.\nPress any key to exit...")
+            os.system("color 0C")
+            msvcrt.getch()
+            exit()
+
         self.FULLNAME_INPUT = f"{self.NAME_INPUT}{self.LASTNAME_INPUT}{randint(1000, 9999)}"
         self.PASSWORD_INPUT = "".join(sample(self.CHARACTERS, 15))
 
@@ -81,24 +90,23 @@ class PotatoGen:
     def driver(self):
         self.DRIVEROPTIONS = ucd.ChromeOptions()
         self.DRIVEROPTIONS.add_argument("--log-level=3")
+        self.DRIVEROPTIONS.add_experimental_option("prefs", self.PREFERENCES)
         if not os.path.exists("chromedriver.exe"):
-            log("Installing Chrome driver")
-            ucd.install("", self.DRIVERVERSION)
-        log("Starting Chrome driver")
+            ucd.install(executable_path="", target_version=self.DRIVERVERSION)
+        log("Starting Chrome Driver")
         self.DRIVER = ucd.Chrome(executable_path=self.DRIVERDIRECTORY, options=self.DRIVEROPTIONS)
-        self.DRIVER.set_window_size(683, 768)
+        self.DRIVER.set_window_size(450, 600)
 
     def generate(self):
         delete(2)
         log("Generating account")
         log("\nIF THE PHONE VERIFICATION SCREEN APPEARS CHANGE IP AND RESTART POTATOGEN")
         log("YOU WILL NEED TO COMPLETE THE BOT VERIFICATION YOURSELF")
-        log("\nIgnore any errors below here\n")
 
         self.DRIVER.get(self.MICROSOFTSIGNUP)
 
         while True:
-            sleep(0.75)
+            sleep(0.1)
             try:
                 if self.DRIVER.find_element(By.NAME, "MemberName").is_displayed() and self.DRIVER.find_element(By.ID, "iSignupAction").is_displayed() and self.STEP == 0:
                     self.DRIVER.find_element(By.NAME, "MemberName").send_keys(self.FULLNAME_INPUT)
@@ -144,6 +152,7 @@ class PotatoGen:
                 if self.DRIVER.find_element(By.ID, "KmsiCheckboxField").is_displayed() and self.DRIVER.find_element(By.ID, "idBtn_Back").is_displayed() and self.STEP == 4:
                     self.DRIVER.find_element(By.ID, "KmsiCheckboxField").click()
                     self.DRIVER.find_element(By.ID, "idBtn_Back").click()
+                    self.DRIVER.close()
 
                     if os.path.exists("accounts.txt"):
                         with open("accounts.txt", "r") as SAVEACCOUNTS:
@@ -163,10 +172,8 @@ class PotatoGen:
                         ACCOUNT.write(f"Password: {self.PASSWORD_INPUT}")
                         ACCOUNT.close()
 
-                    sleep(1)
-
                     os.system("color 0A")
-                    log("\nSuccessfully generated a new Microsoft account!")
+                    log("\nSuccessfully generated a new Microsoft account!\nPress any key to exit...")
                     break
             except (Exception,):
                 pass
@@ -175,13 +182,14 @@ class PotatoGen:
         setup()
 
         if input("Do you want to generate a new Microsoft account? (y, n): ").lower() != "y":
-            quit()
+            exit()
 
         self.credentials()
         self.driver()
         self.generate()
 
-        input()
+        msvcrt.getch()
+        exit()
 
 
 PotatoGen()
